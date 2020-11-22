3bd26dcfab714614e1609c958bb65f889a88ef92
ahhTou <1197977498@qq.com>
2020-11-22 23:00:06 +0800

    微信小程序 第四天 React第一天 入门案例 井字棋

---
+++ React/React.md
@@ -0,0 +1,21 @@
+# React 使用
+
+引入
+
+```html
+<!-- 加载 React。-->
+<!-- 注意: 部署时，将 "development.js" 替换为 "production.min.js"。-->
+<script src="https://unpkg.com/react@16/umd/react.development.js" crossorigin></script>
+<script src="https://unpkg.com/react-dom@16/umd/react-dom.development.js" crossorigin></script>
+```
+
+
+
+启动项目
+
+```BASH
+npx create-react-app my-app
+cd my-app
+npm start
+```
+
---
+++ WeChat/4/components/_index/index-recommend-page/IndexRecommendPage.js
@@ -0,0 +1,33 @@
+// components/_index/index-recommend-page/IndexRecommendPage.js
+Component({
+  /**
+   * 组件的属性列表
+   */
+  properties: {
+
+    // banner-block
+    imgList: {
+      type: JSON,
+      value: [{
+        id: 0,
+        title: "未定义",
+        className: '',
+        url: 'https://s3.ax1x.com/2020/11/20/DQrl9J.jpg'
+      }]
+    }
+  },
+
+  /**
+   * 组件的初始数据
+   */
+  data: {
+
+  },
+
+  /**
+   * 组件的方法列表
+   */
+  methods: {
+
+  }
+})
